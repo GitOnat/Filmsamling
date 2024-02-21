200da74ec2c2bfc8068bad984a5385c69bf7348d
GitOnat <maon0001@stud.kea.dk>
2024-02-21 10:49:35 +0100

    Filmsamling del 1 (done)

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,34 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        MovieCollection myCollection = new MovieCollection();
+
+        System.out.println("To add a movie to your movie collection, follow the instructions below.");
+
+        System.out.println("Movie title:");
+        String title = scanner.nextLine();
+
+        System.out.println("Director name:");
+        String director = scanner.nextLine();
+
+        System.out.println("Release year:");
+        int yearCreated = scanner.nextInt();
+
+        System.out.println("Is the movie in color? (type true for yes or false for no):");
+        boolean isInColor = scanner.nextBoolean();
+
+        System.out.println("Movie length in minutes:");
+        int lengthInMinutes = scanner.nextInt();
+
+        System.out.println("Movie genre:");
+        String genre = scanner.next();
+
+        myCollection.addMovie(title, director, yearCreated, isInColor, lengthInMinutes, genre);
+
+        System.out.println("Movie successfully added to your movie collection.");
     }
 }
